354c58318de8a66ca12ee4d04442ce3a6b469721
任静茹 <2960360155@qq.com>
2023-10-13 14:26:39 +0800

    '项目已完成'

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function downloadClose() {
     var download = document.querySelector('.download');
     var close_ = document.querySelector('.close');
-    console.log(download);
+    // console.log(download);
     download.parentNode.removeChild(download);
     close_.parentNode.removeChild(close_);
 }
--- my/js/rem.js
+++ my/js/rem.js
@@ -4,7 +4,7 @@
     var timeoutId;
     function setRem() {
         var clientWidth = document.documentElement.clientWidth;
-        var nowPX = clientWidth / 320 * 10;
+        var nowPX = clientWidth / 320 * 100;
         document.documentElement.style.fontSize = nowPX + 'px';
     }
     setRem();
@@ -15,26 +15,3 @@
         }, 300);
     }, false);
 })();
-
-
-
-// 轮播图 部分
-var mySwiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
-    loop: true,
-
-    // 如果需要分页器
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-
-
-    },
-
-    autoplay: {
-        delay: 1000,
-        stopOnLastSlide: false,
-        disableOnInteraction: true,
-    },
-})
